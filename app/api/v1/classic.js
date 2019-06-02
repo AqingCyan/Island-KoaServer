53fec69f378b9bddb0f636811f7d735693c7ee2e
AqingCyan <aqingcyan@cyanthing.com>
2019-06-03 00:56:10 +0800

    feat(user.js): 邮箱重复的验证

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -7,14 +7,14 @@ const router = new Router()
 const { PositiveIntegerValidator } = require('../../validators/validator')
 
 // 若想在url中间传参，就可以使用:id的形式
-router.post('/v1/:id/classic/latest', (ctx, next) => {
+router.post('/v1/:id/classic/latest', async (ctx, next) => {
   const path = ctx.params
   const query = ctx.request.query
   const header = ctx.request.header
   const body = ctx.request.body
 
   // 使用定义的参数校验器校验参数
-  const v = new PositiveIntegerValidator().validate(ctx)
+  const v = await new PositiveIntegerValidator().validate(ctx)
   // 获取http参数，parsed为false不进行类型的转换
   const id = v.get('path.id', parsed = false)
   ctx.body = `success, param's id is ${id}`
--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -4,15 +4,24 @@
 
 const Router = require('koa-router')
 const { RegisterValidator } = require('../../validators/validator')
+const { User } = require('../../model/user')
 const router = new Router({
   prefix: '/v1/user'
 })
 
 /**
  * 注册用户
+ * 注册信息校验 -> 获取注册信息，并保存信息到数据库 -> 响应信息
  */
 router.post('/register', async (ctx) => {
-  const v = new RegisterValidator().validate(ctx)
+  const v = await new RegisterValidator().validate(ctx)
+  const user = {
+    email: v.get('body.email'),
+    password: v.get('body.password2'),
+    nickname: v.get('body.nickname')
+  }
+  const r = await User.create(user)
+
 })
 
 module.exports = router
