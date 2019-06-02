5e0e7b9c37386a3b1f284aca91062c4c6130e114
AqingCyan <aqingcyan@cyanthing.com>
2019-06-02 15:23:11 +0800

    feat(user.js): 注册api实现

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -3,11 +3,16 @@
  */
 
 const Router = require('koa-router')
-const router = new Router()
+const { RegisterValidator } = require('../../validators/validator')
+const router = new Router({
+  prefix: '/v1/user'
+})
 
 /**
  * 注册用户
  */
-router.post('/v1/user/register', async (ctx) => {
-
+router.post('/register', async (ctx) => {
+  const v = new RegisterValidator().validate(ctx)
 })
+
+module.exports = router
