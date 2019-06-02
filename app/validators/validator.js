32b04fbc3abcbe62f981bdd1eb8690ab0a48cdf9
AqingCyan <aqingcyan@cyanthing.com>
2019-06-02 15:22:38 +0800

    feat(validator): 注册信息校验

--- app/validators/validator.js
+++ app/validators/validator.js
@@ -1,9 +1,12 @@
 /**
- * 参数校验器（引用lin-validator函数）
+ * 路由参数校验器（引用lin-validator）
  */
 
 const { LinValidator, Rule } = require('../../core/lin-validator')
 
+/**
+ * 路径参数校验器
+ */
 class PositiveIntegerValidator extends LinValidator {
   constructor() {
     super()
@@ -14,6 +17,38 @@ class PositiveIntegerValidator extends LinValidator {
   }
 }
 
+/**
+ * 注册信息校验器
+ */
+class RegisterValidator extends LinValidator {
+  constructor() {
+    super()
+    this.email = [
+      new Rule('isEmail', '不符合e-mail规范')
+    ]
+    this.password = [
+      new Rule('isLength', '密码最少6位，最长32位', { min: 6, max: 32 }),
+      new Rule('matches', '密码不符合规范', '^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]')
+    ]
+    this.password2 = this.password1
+    this.nickname = [
+      new Rule('isLength', '昵称不符合长度规范', { min: 4, max: 32 })
+    ]
+  }
+  /**
+   * 验证两密码是否相等
+   * @param{object} vals 所有注册信息
+   */
+  validatePassword(vals) {
+    const psw1 = vals.body.password
+    const psw2 = vals.body.password2
+    if (psw1 !== psw2) {
+      throw Error('两个密码必须相同')
+    }
+  }
+}
+
 module.exports = {
-  PositiveIntegerValidator
+  PositiveIntegerValidator,
+  RegisterValidator
 }
