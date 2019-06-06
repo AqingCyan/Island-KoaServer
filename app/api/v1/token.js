6773817360abba6c05e0812960260ef2a4dbe38f
AqingCyan <aqingcyan@cyanthing.com>
2019-06-06 23:21:08 +0800

    feat(token.js): 验证token接口

--- app/api/v1/token.js
+++ app/api/v1/token.js
@@ -3,10 +3,13 @@
  */
 
 const Router = require('koa-router')
+const { TokenValidator } = require('../../validators/validator')
 const router = new Router({
   prefix: '/v1/token'
 })
 
-router.post('/', async () => {
-
+router.post('/', async ctx => {
+  const v = await new TokenValidator().validate(ctx)
 })
+
+module.exports = router
