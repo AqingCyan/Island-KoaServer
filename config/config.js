ca418e245d08a3b0aa3b59ef0e6f3daa57abe1f5
AqingCyan <xuekaiqi@creatshare.com>
2019-06-30 18:19:06 +0800

    feat(config): 完成jwt的生成

--- config/config.js
+++ config/config.js
@@ -3,13 +3,18 @@
  */
 module.exports = {
   // 环境参数
-  enviroment: 'dev',
+  enviroment: "dev",
   // 数据库参数
   database: {
-    dbName: 'island',
-    host: 'localhost',
+    dbName: "island",
+    host: "localhost",
     port: 3306,
-    user: 'root',
-    password: 'xkq199862'
+    user: "root",
+    password: "xkq199862"
+  },
+  // 定义jwt中的key字段
+  security: {
+    secretKey: "aqingcyan",
+    expiresIn: 60 * 60
   }
 }
